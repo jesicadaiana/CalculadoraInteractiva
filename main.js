9c6699063cc15b297e93ddb2ba01b0b70945b6b4
jesicadaiana <ajesicadaiana@gmail.com>
2023-11-29 11:55:32 -0300

    Cambios en background

--- main.js
+++ main.js
@@ -22,3 +22,14 @@ function calculate() {
 function updateDisplay() {
     document.getElementById('display').innerText = displayValue;
 }
+
+let nro1 =  Number.parseInt("Ingrese un numero")
+let nro2 = Number.parseInt("Ingrese un numero") 
+
+function Suma (nro1, nro2) {
+    console.log ("La suma es: ", nro1 +  nro2);
+}
+
+function calcularIMC (estatura, peso) {
+    
+}
