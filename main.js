4668be30cd025c72d2e9699efbc43b34df0145b7
jesicadaiana <ajesicadaiana@gmail.com>
2023-11-29 18:22:51 -0300

    m

--- main.js
+++ main.js
@@ -23,13 +23,16 @@ function updateDisplay() {
     document.getElementById('display').innerText = displayValue;
 }
 
-let nro1 =  Number.parseInt("Ingrese un numero")
-let nro2 = Number.parseInt("Ingrese un numero") 
 
-function Suma (nro1, nro2) {
-    console.log ("La suma es: ", nro1 +  nro2);
-}
 
-function calcularIMC (estatura, peso) {
-    
+function calcularIMC() {
+    let estatura = document.getElementById("buttonPrimerNro").value;
+    let peso = document.getElementById("buttonSegundoNro").value;
+        if (estatura && peso) {     // Verifica que ambos campos tengan valores
+            let imc = peso / (estatura * estatura);
+            document.getElementById("resultado_imc").innerText = imc.toFixed(1);
+            console.log("El IMC es: " + imc.toFixed(1));
+        } else {
+            alert("Por favor, complete ambos campos.");
+        }
 }
